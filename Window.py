b1e0198496426db2a0cc09804d3d7418c3269e4b
Jessin Jai Abraham <jessinj2@gmail.com>
2022-09-09 15:50:00 -0400

    player2
    player2 is been added

--- Window.py
+++ Window.py
@@ -12,13 +12,23 @@ screen = pygame.display.set_mode((800,600))
 pygame.display.set_caption("A-T Robot")
 icon = pygame.image.load("robots01.gif")
 
-#player
+#player1
 player1Img = pygame.image.load("tank.png")
 player1X = random.randint(32,768)
-player1Y = random.randint(32,668)
+player1Y = random.randint(32,568)
+print(player1Y, player1X)
+#player2
 
-def player(x, y):
-    screen.blit(player1Img, (x,y))
+player2Img = pygame.image.load("tank2.png")
+player2X = random.randint(32+20, random.randint(32,768))
+player2Y = random.randint(32+20, random.randint(32,568))
+print(player2Y, player2X)
+#player3
+
+
+def player(playerImage, x, y):
+    screen.blit(playerImage, (x,y))
+
 
 #Game Loop
 running = True
@@ -31,5 +41,6 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    player(player1X, player1Y)
+    player(player1Img, player1X, player1Y)
+    player(player2Img, player2X, player2Y)
     pygame.display.update()
