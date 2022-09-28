fd19e2e30696adaf2d326b5aa8dddb0cf442b9ac
JarrettIannotti <80851453+JarrettIannotti@users.noreply.github.com>
2022-09-28 13:37:20 -0400

    Movement fixes3

--- Player.py
+++ Player.py
@@ -2,19 +2,21 @@ import pygame
 import math
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self,color,Xcoord,Ycoord,health):
+    def __init__(self,color,Xcoord,Ycoord,health):      #Set by game initializer, initiates a player object
         self.color = color
         self.Xcoord = Xcoord
         self.Ycoord = Ycoord
-        self.health = 100
+        self.health = health
+        #might need more settings/config. Also this init could handle sprites or it can be a seperate file?
+    def ImHit(self):                                    #Method for getting hit
+        self.health =- 10
 
-    def ImHit(self):
-        health =- 10
+    def Movement(self,angle):                           #Moves Player
+        self.Xcoord += math.cos(angle)
+        self.Ycoord += math.sin(angle)
+            #Need to add collision detection
 
-    def Movement(self,angle):
-        Xcoord += cos(angle)
-        Ycoord += sin(angle)
 
 # Add these all to a group eventually with all_sprites_list = pygame.sprite.Group()
 # Then all_sprites_list.add(object_) ON https://www.geeksforgeeks.org/pygame-creating-sprites/
-#https://stackoverflow.com/questions/61088785/pygame-trying-to-understand-the-sprite-class
+# https://stackoverflow.com/questions/61088785/pygame-trying-to-understand-the-sprite-class
